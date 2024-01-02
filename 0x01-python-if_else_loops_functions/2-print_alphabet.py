d109c7dbaee6a338890078b96f95e9fcb9273918
DerrickMits <derrickodiwuor@gmail.com>
2024-01-02 07:55:55 -0800

    prints the ASCII alphabet, in lowercase

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-for char in range(97, 123):
-    print(chr(char).format(), end='')
+for char in range(ord('a'), ord('z') + 1):
+    print("{:c}".format(char), end='')
 
 print()
