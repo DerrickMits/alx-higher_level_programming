d7c4f572e2592dc3076036ee9d8f7fd1676cc61d
DerrickMits <derrickodiwuor@gmail.com>
2024-01-04 09:45:14 -0800

    imports all functions from the file calculator_1.py and handles basic operations

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,20 +1,34 @@
 #!/usr/bin/python3
+"""Handles basic arithmetic operations."""
+from calculator_1 import add, sub, mul, div
+import sys
 
 if __name__ == "__main__":
-    """Handle basic arithmetic operations."""
-    from calculator_1 import add, sub, mul, div
-    import sys
-
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
-    a, operator, b = map(int, sys.argv[1:])
-    
-    ops = {"+": add, "-": sub, "*": mul, "/": div}
-    if operator not in ops:
+    a, operator, b = sys.argv[1], sys.argv[2], sys.argv[3]
+
+    if not (a.isdigit() or (a[0] == '-' and a[1:].isdigit())):
+        print(f"Invalid value for <a>: {a}")
+        sys.exit(1)
+
+    if operator not in ('+', '-', '*', '/'):
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
 
-    result = ops[operator](a, b)
-    print(f"{a} {operator} {b} = {result}")
+    if not (b.isdigit() or (b[0] == '-' and b[1:].isdigit())):
+        print(f"Invalid value for <b>: {b}")
+        sys.exit(1)
+
+    a, b = int(a), int(b)
+
+    result = {
+        '+': add,
+        '-': sub,
+        '*': mul,
+        '/': div
+    }[operator](a, b)
+
+    print("{} {} {} = {}".format(a, operator, b, result))
