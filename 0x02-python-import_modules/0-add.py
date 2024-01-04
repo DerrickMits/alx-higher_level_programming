7ef72d3ba0fa4564cd42d616dd04dc59dffa9691
DerrickMits <derrickodiwuor@gmail.com>
2024-01-04 01:21:14 -0800

    Program that imports the the function prints the resultd of addition 1 + 2 = 3

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    """the sum of 1 and 2."""
-a = 1
-b = 2
-from add_0 import add
+    """The sum of 1 and 2."""
+    a = 1
+    b = 2
+    from add_0 import add
 
-print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    result = add(a, b)
+    print("{:d} + {:d} = {:d}".format(a, b, result))
