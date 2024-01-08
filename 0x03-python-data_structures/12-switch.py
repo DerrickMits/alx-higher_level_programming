60bf0f251eba5d9da27029507b0439c8fe3c1ddd
DerrickMits <derrickodiwuor@gmail.com>
2024-01-08 12:36:44 -0800

    source code in order to switch value of a and b

--- 0x03-python-data_structures/12-switch.py
+++ 0x03-python-data_structures/12-switch.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
 """
 Switch the values of variables a and b.
-
-This script initializes two variables, a and b
-It then switches their values and prints the result
-
 """
-a, b = 89, 10
+a = 89
+b = 10
 a, b = b, a
 print("a={:d} - b={:d}".format(a, b))
