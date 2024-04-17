61f88cff3a92d49ef4359e56015f7a426be3062f
DerrickMits <derrickodiwuor@gmail.com>
2024-04-17 05:44:09 -0700

    script that lists all states from the database hbtn_0e_0_usa

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,17 +1,13 @@
 #!/usr/bin/env python3
+#script that lists all states from the database hbtn_0e_0_usa
 import MySQLdb
 import sys
 
 
 if __name__ == "__main__":
-    # Extracting MySQL username, password, and database name
-    username = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
-
     # Connecting to the MySQL server
     db = MySQLdb.connect(
-        host="localhost", port=3306, user=username, passwd=password, db=db_name
+        host="localhost", port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3]
     )
     cursor = db.cursor()
 
