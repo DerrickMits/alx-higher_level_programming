acef4233d67e02210086aa8454a23d9ea016a2e6
DerrickMits <derrickodiwuor@gmail.com>
2024-04-25 10:14:04 -0700

    Bash script that sends a JSON POST request to a URL passed as the first argument, and displays the body of the response.

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-#bash script
-# Check if the file exists
-if [ ! -f "$2" ]; then
-    echo "File $2 does not exist."
-    exit 1
-fi
-# Send the POST request and display the body of the response
+# sends a JSON POST request to a URL passed , and displayed
 curl -sX POST -H "Content-Type: application/json" -d @"$2" "$1"
