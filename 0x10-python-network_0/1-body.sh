96b1989ed8ebddc590afd92c5439bc85f322a94e
DerrickMits <derrickodiwuor@gmail.com>
2024-04-25 02:25:28 -0700

    Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,16 +1,3 @@
 #!/bin/bash
-
-URL="$1"
-
-if [ -z "$URL" ]
-then
-  echo "Usage: $0 <URL>"
-  exit 1
-fi
-
-response=$(curl -s -o /dev/null -w "%{http_code}" "$URL")
-
-if [ "$response" == "200" ]
-then
-  curl -s "$URL"
-fi
+# This script takes in a URL, sends a GET request, and displays(body)
+curl -s -X GET "$1" -D - | awk '/HTTP\/1.1 200 OK/ {flag=1; next} flag {print}'
